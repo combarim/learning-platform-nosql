5b22603dabd7fbb9c5d83e542a6816de43a14fb3
combarim <43067163+combarim@users.noreply.github.com>
2025-01-08 21:32:25 +0100

    Mise en place de la validation des variables d'environnement

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 // Question: Pourquoi est-il important de valider les variables d'environnement au démarrage ?
-// Réponse : 
+// Réponse : parce que si une variable d'environnement est manquante ou invalide, l'application ne fonctionnera pas correctement
 // Question: Que se passe-t-il si une variable requise est manquante ?
-// Réponse : 
+// Réponse : si une variable requise est manquante, une erreur sera levée
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -12,11 +12,43 @@ const requiredEnvVars = [
   'REDIS_URI'
 ];
 
+
 // Validation des variables d'environnement
 function validateEnv() {
-  // TODO: Implémenter la validation
-  // Si une variable manque, lever une erreur explicative
+
+  //vérifier que tous les variables d'environnement sont définies
+  for (const envVar of requiredEnvVars) {
+    if (!process.env[envVar]) {
+      throw new Error(`Il manque la variable d'environnement: ${envVar}`);
+    }
+  }
+
+  const mongodbUriRegex = /^(mongodb(?:\+srv)?:\/\/)([a-zA-Z0-9\-_.]+)(?::([0-9]+))?(?:\/([a-zA-Z0-9\-_.]+))?(?:\?(.+))?$/;
+  const redisUriRegex = /^redis:\/\/(?:[a-zA-Z0-9._%+-]+:[^@]+@)?[a-zA-Z0-9.-]+(?::\d+)?(?:\/\d+)?$/ 
+  const mongodbDbNameRegex = /^(?![0-9]\.)(?!.*[\\"/$\0]).{1,64}$/
+  const mongodbDbName = process.env.MONGODB_DB_NAME
+
+  //vérifie si mongodbUri est de la forme : mongodb[+srv]://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]
+  if(mongodbUriRegex.test(process.env.MONGODB_URI) === false) {
+    throw new Error('MONGODB_URI n\'est pas valide');
+  }
+
+  //vérifie si redisUri est de la forme : redis://[username:password@]host[:port][/db-number]
+  if(redisUriRegex.test(process.env.REDIS_URI) === false) {
+    throw new Error('REDIS_URI n\'est pas valide');
+  }
+
+  //si la variable est vide
+  if(mongodbDbName.length === 0) {
+    throw new Error('MONGODB_DB_NAME ne doit pas être vide');
+  }
+
+  //si la variable contient des caractères interdits
+  if(mongodbDbNameRegex.test(mongodbDbName) === false) {
+    throw new Error('MONGODB_DB_NAME n\'est pas valide');
+  }
 }
+
 
 module.exports = {
   mongodb: {
